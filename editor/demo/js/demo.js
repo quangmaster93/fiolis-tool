d8c0baeb966d5e0cb12b57ddae8821113bd453f9
ducdk90 <ducdk90@gmail.com>
2019-11-24 14:12:35 +0700

    Integrate API for get info of land and its adjacent lands

--- editor/demo/js/demo.js
+++ editor/demo/js/demo.js
@@ -1,8 +1,15 @@
-var geojsonText = document.getElementById('geojson-text');
+var sheetText = document.getElementById('sheet-text');
+var parcelText = document.getElementById('parcel-text');
 var btnConvert = document.getElementById('btn-convert');
 btnConvert.addEventListener('click', convert);
 
 function convert() {
+  var sheetNum = sheetText.value;
+  var parcelNum = parcelText.value;
+  convertLandInfo(sheetNum, parcelNum);
+}
+
+const convertGeojsonToSvg = function(geojson) {
   var option = {
     size: [512, 512],           // size[0] is svg width, size[1] is svg height
     padding: [10, 10, 10, 10],  // paddingTop, paddingRight, paddingBottom, paddingLeft, respectively
@@ -16,12 +23,7 @@ function convert() {
     radius: 5                   // only for `Point`, `MultiPoint`
   };
 
-  // svgContainer.innerHTML = '';
-  var geojsonStr = geojsonText.value;
-  var optionStr = JSON.stringify(option);
-  if (geojsonStr) {
-    var geojson = JSON.parse(removeSpace(geojsonStr));
-    var option = JSON.parse(removeSpace(optionStr));
+  if (geojson) {
     svgData = geojson2svg(geojson, option);
 
     // After convert geojson to svg then open svg editor dialog
@@ -41,6 +43,27 @@ function convert() {
     $('svg-editor').css({ display: "block" });
   }
 }
+
+const convertLandInfo = function(sheetNum, parcelNum) {
+  var request = new XMLHttpRequest();
+  const key = `8bd33b7fd36d68baa96bf446c84011da`;
+  request.open('GET', `https://api-fiolis.map4d.vn/v2/api/land/find-adjacent?code=001001003003&soTo=${sheetNum}&soThua=${parcelNum}&key=${key}`, true);
+  request.onload = function() {
+    // Begin accessing JSON data here
+    var data = JSON.parse(this.response);
+
+    if (request.status >= 200 && request.status < 400 &&
+      data.result && data.result.features && data.result.features.length > 1) {
+      // Only get geojson with format wgs84
+      data.result.features.length = data.result.features.length / 2;
+      convertGeojsonToSvg(data.result);
+    } else {
+      console.log('Occur error when request API');
+    }
+  }
+
+  request.send();
+};
 
 function removeSpace(str) {
   return str.split('').filter(function (item) {
